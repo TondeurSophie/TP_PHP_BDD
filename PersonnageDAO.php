8a7085150d90b0b90c1496a9c53534bc3079a241
Prapaline <124569549+Prapaline@users.noreply.github.com>
2023-11-23 12:23:09 +0100

    Add files via upload
    Ajout des armes au niveau de la classe + possibilité d'ajouter/Afficher les armes et afficher uniquement les armes utilisables ne fonction du niveau du joueur

--- PersonnageDAO.php
+++ PersonnageDAO.php
@@ -1,9 +1,86 @@
 <?php
-class PersonnageDAO {
+class DAO{
     private $bdd;
 
     public function __construct($bdd) {
         $this->bdd = $bdd;
+    }
+
+    public function ajouterPersonnage(Personnages $personnages) {
+        try {
+            //Requête SQL permettant d'insérer un personnage dans la BDD avec ses attributs (nom, PV,PA,PD,exp_donne,niveau)
+            $requete = $this->bdd->prepare("INSERT INTO personnage (nom, PV,PA,PD,exp_donne,niveau) VALUES (?,?,?,?,?,?)");
+            // création du personnage avec les valeurs de l'objet Utilisateur
+            $requete->execute([$personnages->getNom(), $personnages->getPV(),$personnages->getPA(),$personnages->getPD(),$personnages->getexp(),$personnages->getNiveau()]);
+            return true; //si tout fonctionne, on revoie true
+        } 
+        //gestion d'erreur
+        catch (PDOException $e) {
+            //message d'erreur
+            echo "Erreur d'ajout du personnage : " . $e->getMessage();
+            //on retourne false
+            return false;
+        }
+    }
+
+    public function listerPersonnage() {
+        try {
+            // requête qui permet de récupérer tous les personnages
+            $requete = $this->bdd->prepare("SELECT * FROM personnage");
+            //execution de la requete précédente
+            $requete->execute();
+            // Retourne un tableau avec les personnages
+            return $requete->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erreur de récupération des personnages : " . $e->getMessage();
+            return [];
+        }
+    }
+
+    public function modifierPersonnage($Pv,$id){
+        try{
+            $requete=$this->bdd->prepare("UPDATE personnage SET Pv = ? WHERE id = ?");
+            $requete->execute([$Pv,$id]);
+            return true;
+        }catch (PDOException $e){
+            echo "Erreur modif salaire ".$e->getMessage();
+        }
+    }
+    public function ajouterArme(Arme $arme) {
+        //Ajout du arme dans la base de données
+        try {
+            $requete = $this->bdd->prepare("INSERT INTO arme (Nom, Niveau_requis, Pv, PA, PD) VALUES (?, ?, ?, ?, ?)");
+            $requete->execute([$arme->getNom(), $arme->getNiveauRequis(), $arme->getPV(), $arme->getPa(), , $arme->getPd()]);
+            return true;
+        } catch (PDOException $e) {
+            echo "Erreur d'ajout de arme: " . $e->getMessage();
+            return false;
+        }
+
+    }
+
+    public function listerArme() {
+        //Liste des armes en selectionnant toute la table
+        try {
+            $requete = $this->bdd->prepare("SELECT * FROM arme");
+            $requete->execute();
+            return $requete->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erreur de récupération des objets : " . $e->getMessage();
+            return [];
+        }
+    }
+
+    public function listerArmeUtilisable(Arme $arme) {
+        try {
+            //Récupération des armes utilisable en fonction du niveau du personnage
+            $requete = $this->bdd->prepare("SELECT * FROM arme WHERE Niveau_requis <= ?");
+            $requete->execute([$arme->getNiveauRequis()]);
+            return $requete->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erreur de récupération des armes: " . $e->getMessage();
+            return [];
+        }
     }
 
     public function ajouterMonstre(Monstre $monstre) {
@@ -81,6 +158,7 @@ class PersonnageDAO {
             return [];
         }
     }
+
     public function mettreAJourPersonnage($idPersonnage, $idNiveau) {
         try {
             //Récupération des informations sur le personnage en fonction de l'id
--- index.php
+++ index.php
@@ -3,4 +3,56 @@ include("config.php");
 include("Personnage.php");
 include("PersonnageDAO.php");
 
+$DAO = new DAO($connexion);
+
+$choix=readline("Que voulez faire ?\n 1. Ajouter un utilisateur\n 2. Lister les personnages \n 3. Jouer \n 4. Quitter \n");
+
+switch ($choix){
+    case "1":
+        $nom=readline("Entrer le nom : ");
+        $PV=readline("Entrer le PV : ");
+        $PA=readline("Entrer le PA : ");
+        $PD=readline("Entrer le PD : ");
+        $exp=readline("Entrer le exp : ");
+        $Perso = new Personnages("",$nom,$PV,$PA,$PD,$exp,1);
+        $DAO->ajouterPersonnage($Perso);
+        echo "Le personnage a bien été ajouté. \n";
+        echo "Liste des personnages : \n";
+        $personnages = $DAO->listerPersonnage();
+        // print_r($personnages);
+        if ($personnages){
+            foreach($personnages as $e){
+                echo "Id : ".$e['Id']."\n";
+                echo 'Nom : '.$e['Nom']."\n";
+                echo 'PV : '.$e['PV']."\n";
+                echo "_ _ _ _ _ _ _ _ _ _ _ _ _ _\n";
+            }
+        }
+
+        break;
+    case"2":
+        echo "Liste des personnages : \n";
+        $personnages = $DAO->listerPersonnage();
+        // print_r($personnages);
+        if ($personnages){
+            foreach($personnages as $e){
+                echo "Id : ".$e['Id']."\n";
+                echo 'Nom : '.$e['Nom']."\n";
+                echo 'PV : '.$e['PV']."\n";
+                echo "_ _ _ _ _ _ _ _ _ _ _ _ _ _\n";
+            }
+        }
+        break;
+
+    case"3":
+
+        break;
+    case"4":
+        echo "Au revoir ! ";
+        break;
+    default:
+    echo "Au revoir ! ";
+        break;
+
+}
 ?>
