e70136fa836dff8e5efa0d060e717eb11f66df61
Prapaline <124569549+Prapaline@users.noreply.github.com>
2023-11-23 11:02:47 +0100

    Add files via upload
    Création des classes pour le monstre et la salle

---
+++ Personnage.php
@@ -0,0 +1,66 @@
+<?php
+class Salle {
+    private $id; //Ne sert a rien car id en auto-increment
+    private $epreuve;
+    private $recompense;
+
+    public function __construct($epreuve, $recompense, $listeArticle,$infoSupplementaire) {
+        $this->epreuve = $epreuve;
+        $this->recompense = $recompense;
+    }
+
+    public function getEpreuve() {
+        return $this->epreuve;
+    }
+
+    public function getRecompense() {
+        return $this->recompense;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+}
+
+class Monstre {
+    private $id; //Ne sert a rien car id en auto-increment
+    private $nom;
+    private $pv;
+    private $pa;
+    private $pd;
+
+    private $exp_donne;
+
+
+    public function __construct($nom, $pv, $pa,$pd,$exp_donne) {
+        $this->nom = $nom;
+        $this->pv = $pv;
+        $this->pa = $pa;
+        $this->pd = $pd;
+        $this->exp_donne = $exp_donne;
+
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function getPv() {
+        return $this->pv;
+    }
+    public function getPa() {
+        return $this->pa;
+    }
+    public function getPd() {
+        return $this->pd;
+    }
+    public function getExpDonne() {
+        return $this->exp_donne;
+    }
+    public function getId() {
+        return $this->id;
+    }
+}
+
+
+?>
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+try {
+    //Données de connexion
+    $hote = "localhost";
+    $utilisateur = "azerty";
+    $motDePasse = "mdp";
+    $nomDeLaBase = "TP3";
+    //Connexion à la base de données
+    $connexion = new PDO("mysql:host=$hote;dbname=$nomDeLaBase", $utilisateur, $motDePasse);
+    
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo"Connection Réussi";
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données: " . $e->getMessage();
+    die();
+}
+    
+
+?>
