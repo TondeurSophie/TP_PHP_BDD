5a3d5169b00f4aa7cefe7512bf8e483dcb5b8e09
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-11-23 11:51:25 +0100

    Ajout partie Morgane

--- DAO.php
+++ DAO.php
@@ -51,6 +51,82 @@ class DAO{
             echo "Erreur modif salaire ".$e->getMessage();
         }
     }
+
+    public function ajouterMonstre(Monstre $monstre) {
+        //Ajout du monstre dans la base de données
+        try {
+            $requete = $this->bdd->prepare("INSERT INTO monstre (Nom, Pv, PA, PD, exp_done) VALUES (?, ?, ?, ?, ?)");
+            $requete->execute([$monstre->getNom(), $monstre->getPV(), $monstre->getPa(), , $monstre->getPd(), , $monstre->getExpDonne()]);
+            return true;
+        } catch (PDOException $e) {
+            echo "Erreur d'ajout de monstre: " . $e->getMessage();
+            return false;
+        }
+
+    }
+    public function trouverMonstreParId($id) {
+        try {
+            //Recherche un monstre en particulier en fonction de l'id
+            $requete = $this->bdd->prepare("SELECT * FROM monstre WHERE id = ?");
+            $requete->execute([$id]);
+            $resultat = $requete->fetch(PDO::FETCH_ASSOC);
+
+            if ($resultat) {
+                return new Monstre($resultat['Nom'], $resultat['Pv'], $resultat['PA'], $resultat['PD'], $resultat['exp_donne']);
+            } else {
+                return null;
+            }
+        } catch (PDOException $e) {
+            echo "Erreur lors de la recherche du personnage par ID: " . $e->getMessage();
+            return null;
+        }
+    }
+    public function ajouterSalle(Salle $salle) {
+        //Ajout des niveau (utilisé qu'au début afin de remplir un minimum la base de données)
+        try {
+            $requete = $this->bdd->prepare("INSERT INTO salle (niveau, epreuve, recompense) VALUES (?, ?)");
+            $requete->execute([$salle->getNiveau(), $salle->getEpreuve(), $salle->getRecompense()]);
+            return true;
+        } catch (PDOException $e) {
+            echo "Erreur d'ajout de la salle : " . $e->getMessage();
+            return false;
+        }
+
+    }
+
+    public function listerMonstres() {
+        //Liste des monstre en selectionnant toute la table
+        try {
+            $requete = $this->bdd->prepare("SELECT * FROM monstre");
+            $requete->execute();
+            return $requete->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erreur de récupération des monstres: " . $e->getMessage();
+            return [];
+        }
+    }
+    public function listerSalleActuelle(Salle $salle) {
+        try {
+            //Récupération des salles disponibles en fonction du niveau du personnage
+            $requete = $this->bdd->prepare("SELECT * FROM salle WHERE niveau = ?");
+            $requete->execute([$salle->getNiveau()]);
+            return $requete->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erreur de récupération des salles: " . $e->getMessage();
+            return [];
+        }
+    }
+    public function listerSalle(Salle $salle) {
+        try {
+            //Récupération de toutes les salles
+            $requete = $this->bdd->prepare("SELECT * FROM salle");
+            $requete->execute();
+            return $requete->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erreur de récupération des salles: " . $e->getMessage();
+            return [];
+        }
+    }
 }
 
 
--- classes.php
+++ classes.php
@@ -43,62 +43,76 @@ class Personnages{
 }
 
 
-class Salles{
+class Salle{
     private $id;
+    private $niveau;
     private $epreuve;
     private $recompense;
 
-    public function __construct($id,$epreuve,$recompense){
+    public function __construct($id,$niveau,$epreuve,$recompense){
         $this->id=$id;
-        $this->epreuve=$epreuve;
-        $this->recompense=$recompense;
+        $this->niveau = $niveau;
+        $this->epreuve = $epreuve;
+        $this->recompense = $recompense;
     }
 
     public function get_Id(){
         return $this->id;
     }
 
-    public function get_epreuve(){
+    public function getNiveau() {
+        return $this->niveau;
+    }
+    public function getEpreuve() {
         return $this->epreuve;
     }
-    public function get_recompense(){
+
+    public function getRecompense() {
         return $this->recompense;
+    }
+
+    public function getId() {
+        return $this->id;
     }
 }
 
 
-class Monstre{
-    private $id;
+class Monstre {
+    private $id; //Ne sert a rien car id en auto-increment
     private $nom;
-    private $PV;
-    private $PA;
-    private $PD;
-    
+    private $pv;
+    private $pa;
+    private $pd;
 
-    public function __construct($id,$nom,$PV,$PA,$PD){
-        $this->id=$id;
-        $this->nom=$nom;
-        $this->PV=$PV;
-        $this->PA=$PA;
-        $this->PD=$PD;
- 
+    private $exp_donne;
+
+
+    public function __construct($nom, $pv, $pa,$pd,$exp_donne) {
+        $this->nom = $nom;
+        $this->pv = $pv;
+        $this->pa = $pa;
+        $this->pd = $pd;
+        $this->exp_donne = $exp_donne;
     }
 
-    public function getId(){
-        return $this->id;
-    }
-    public function getNom(){
+    public function getNom() {
         return $this->nom;
     }
-    public function getPV(){
-        return $this->PV;
+
+    public function getPv() {
+        return $this->pv;
     }
-    public function getPA(){
-        return $this->PA;
+    public function getPa() {
+        return $this->pa;
     }
-    public function getPD(){
-        return $this->PD;
+    public function getPd() {
+        return $this->pd;
     }
-    
+    public function getExpDonne() {
+        return $this->exp_donne;
+    }
+    public function getId() {
+        return $this->id;
+    }
 }
 ?>
